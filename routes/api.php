16100d39b335f48911820ebf933bc0f44b5f9159
LiamPorton <69626686+LiamPorton@users.noreply.github.com>
2021-02-22 14:19:46 +0100

    Back API routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Contact;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,39 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// Fetch Contacts
+Route::group(['middleware' => 'api'], function(){
+    Route::get('contacts', function(){
+        return Contact::latest()->orderBy('created_at', 'desc')->get();
+    });
+});
+
+// Get a single contact by his id
+Route::get('contact/{id}', function($id){
+    return Contact::findOrFail($id);
+});
+
+// Add a contact
+Route::post('contact/store', function(Request $request){
+    return Contact::create([
+        'name' => $request->input(['name']),
+        'email' => $request->input(['email']),
+        'phone' => $request->input(['phone'])]);
+});
+
+// Update a contact
+Route::patch('contact/{id}', function(Request $request, $id){
+    Contact::findOrFail($id)->update([
+    'name' => $request->input(['name']),
+    'email' => $request->input(['email']),
+    'phone' => $request->input(['phone'])]);
+});
+
+// Delete a contact
+Route::delete('contact/{id}', function($id){
+    return Contact::destroy($id);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
