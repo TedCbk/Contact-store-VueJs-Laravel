cea8dd75b43bcac6ca01f6519e51ce825ccaaa24
LiamPorton <69626686+LiamPorton@users.noreply.github.com>
2021-02-22 14:33:51 +0100

    Creating a Vue Component

---
+++ resources/js/components/Contacts.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Contacts</h1>
+</template>
+
+<script>
+    export default{
+    data: function(){
+        return {}
+    },
+    mounted: function(){
+    console.log('Contacts Components Loaded...')}
+    }
+</script>
